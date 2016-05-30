0a4461ffb3c71f3abc756d158b1312c21e855370
ppawelqq1 <pejaneczek@gmil.com>
2016-05-30 20:21:40 +0200

    first commit

--- index.php
+++ index.php
@@ -9,14 +9,14 @@
     <link rel="icon" href="../../favicon.ico">
 
     <title>SKLEP INTERNETOWY</title>
-      
+
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <link href="http://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <link href="style.css" rel="stylesheet">
-      
+
     <script src="./scripts/check.js"></script>
 
   </head>
@@ -26,8 +26,8 @@
     <div class="container">
       <form class="form-signin">
         <h2 class="form-signin-heading">Logowanie</h2>
-        <label for="inputEmail" class="sr-only">E-mail</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="E-mail" required autofocus>
+        <label for="inputEmail" class="sr-only">Login</label>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Login" required autofocus>
         <label for="inputPassword" class="sr-only">Hasło</label>
         <input type="password" id="inputPassword" class="form-control" placeholder="Hasło" required>
         <div class="checkbox">
--- scripts/check.js
+++ scripts/check.js
@@ -3,16 +3,19 @@
  */
 $(document).ready(function() {
     $("#login").click(function() {
-        //TODO z poziomu nodejs + mongoDB
-        
-        // Z poziomu cookies
         var cookieUser = document.cookie.match(/USER=(.+)#/),
             cookieUserValue = cookieUser[0].substring(5, cookieUser[0].length),
-            cookieUserValueArr = cookieUserValue.split("*");
-         
+            cookieUserValueArr = cookieUserValue.split("*"), userDetails = {};
+
+            userDetails.imie = cookieUserValueArr[0];
+            userDetails.nazwisko = cookieUserValueArr[1];
+            userDetails.imie = cookieUserValueArr[2];
+
+            localStorage.setItem('userDetails', JSON.stringify(userDetails));
+
         if (($("#inputEmail").val() === cookieUserValueArr[2]) &&
             ($("#inputPassword").val() === cookieUserValueArr[3])) {
-            
+
             parent.location = "/home.html";
         } else {
             alert("Niepoprawny email lub hasło");
@@ -43,9 +46,8 @@ $(document).ready(function() {
  */
 $(document).ready(function() {
     $("#register_me").click(function() {
-        //TODO z poziomu nodejs + mongoDB
 
-        //z poizomu cookies
+        //z poziomu cookies
         var expires = new Date(),
             userData = "";
 
@@ -55,7 +57,193 @@ $(document).ready(function() {
             $("#inputEmail").val() + "*" + $("#inputPassword").val() + "*#";
 
         document.cookie = "USER" + '=' + userData + ';expires=' + expires.toUTCString();
-
         parent.location = '/';
+    });
+});
+/**
+ * Autouzupełnie koszyka na podstawie localStorage
+ */
+var _completionCart = function() {
+    var i, localStorageCartItem = [],
+        sumPrice = 0;
+
+    if (localStorage.getItem("localStorageCart") && localStorage.getItem("localStorageCart").length > 0) {
+        localStorageCartItem = JSON.parse(localStorage.getItem("localStorageCart"));
+
+        if (($('#cart_menu')[0].children && localStorageCartItem)) {
+
+            $('#cart_menu').empty();
+            for (i = 0; i < localStorageCartItem.length; i++) {
+
+                sumPrice += parseFloat(parseFloat(Math.round(localStorageCartItem[i].price * 100) / 100).toFixed(2));
+
+                $('#cart_menu').append(
+                    "<li>" +
+                    "<span class='item' style='width:200px;padding-top:10px;padding-bottom:10px;padding-left:15px;padding-right:20px'>" +
+                    "<span class='item-left'>" +
+                    "<img src='" + localStorageCartItem[i].imgLink + "' style='width:50px;height:50px'alt=''/>" +
+                    "<span class='item-info'>" +
+                    "</span>" +
+                    "</span>" +
+                    "<span class='item-right'>" +
+                    "<span>" + "   " + localStorageCartItem[i].name + "  </span>" +
+                    "<input type='hidden' name='price' value='49.99' />" +
+                    "</span>" +
+                    "<button type='button' id='" + localStorageCartItem[i].imgLink + "' class='btn btn-default' style='position:absolute;left:350px;' default='remove_from_cart'><span class='glyphicon glyphicon-remove' aria-hidden='true'></span></button>" +
+                    "</span>" +
+                    "</li>"
+
+                );
+            }
+            $('#cart_menu').append(
+                "<li>" +
+                "<span class='item' style='width:200px;padding-top:30px;padding-bottom:10px;padding-left:15px;padding-right:20px'>" +
+                "<span class='item-left'>" +
+                "<span class='item-info'>" +
+                "</span>" +
+                "</span>" +
+                "<span class='item-right'>" +
+                "<span></span>" +
+                "</span>" +
+                "</span>" +
+                "</li>"
+            );
+            $('#cart_menu').append(
+                "<li>" +
+                "<span class='item' style='width:200px;padding-top:30px;padding-bottom:10px;padding-left:200px;padding-right:20px'>" +
+                "<span class='item-left'>" +
+                "<img src='./public/podsumowanie.jpg' style='width:50px;height:50px'alt=''/>" +
+                "<span class='item-info'>" +
+                "</span>" +
+                "</span>" +
+                "<span class='item-right'>" +
+                "<span> Razem: " + sumPrice.toFixed(2) + " zł</span>" +
+                "</span>" +
+                "</span>" +
+                "</li>"
+            );
+        }
+    }
+};
+$(document).delegate("*[default='remove_from_cart']", 'click', function() {
+
+    var i, itemImg = $(this).attr('id'),
+        localStorageCart = JSON.parse(localStorage.getItem("localStorageCart")),
+        newLocalStorage = [];
+
+    for (i = 0; i < localStorageCart.length; i++) {
+        if (localStorageCart[i].imgLink !== itemImg) {
+            newLocalStorage.push(localStorageCart[i]);
+        }
+    }
+
+    localStorage.setItem('localStorageCart', JSON.stringify(newLocalStorage));
+
+    _completionCart();
+});
+/**
+ * Obsługa kliknięcia w menu kontekstowe
+ */
+$(document).ready(function() {
+    // menu swimming
+    $("#menu_swimming").click(function() {
+        parent.location = '/shop.html';
+    });
+
+    $("#menu_swimming").mouseenter(function() {
+        $("#menu_swimming").css({
+            opacity: 1
+        });
+    });
+
+    $("#menu_swimming").mouseleave(function() {
+        $("#menu_swimming").css({
+            opacity: 0.4
+        });
+    });
+
+    //menu running
+    $("#menu_running").click(function() {
+        alert("aaa");
+    });
+
+    $("#menu_running").mouseenter(function() {
+        $("#menu_running").css({
+            opacity: 1
+        });
+    });
+
+    $("#menu_running").mouseleave(function() {
+        $("#menu_running").css({
+            opacity: 0.4
+        });
+    });
+
+    // menu bike
+    $("#menu_bike").click(function() {
+        alert("aaa");
+    });
+
+    $("#menu_bike").mouseenter(function() {
+        $("#menu_bike").css({
+            opacity: 1
+        });
+    });
+
+    $("#menu_bike").mouseleave(function() {
+        $("#menu_bike").css({
+            opacity: 0.4
+        });
+    });
+
+    //menu roll
+    $("#menu_roll").click(function() {
+        alert("aaa");
+    });
+
+    $("#menu_roll").mouseenter(function() {
+        $("#menu_roll").css({
+            opacity: 1
+        });
+    });
+
+    $("#menu_roll").mouseleave(function() {
+        $("#menu_roll").css({
+            opacity: 0.4
+        });
+    });
+
+    //menu ski
+    $("#menu_ski").click(function() {
+        alert("aaa");
+    });
+
+    $("#menu_ski").mouseenter(function() {
+        $("#menu_ski").css({
+            opacity: 1
+        });
+    });
+
+    $("#menu_ski").mouseleave(function() {
+        $("#menu_ski").css({
+            opacity: 0.4
+        });
+    });
+
+    //menu snowboard
+    $("#menu_snowboard").click(function() {
+        alert("aaa");
+    });
+
+    $("#menu_snowboard").mouseenter(function() {
+        $("#menu_snowboard").css({
+            opacity: 1
+        });
+    });
+
+    $("#menu_snowboard").mouseleave(function() {
+        $("#menu_snowboard").css({
+            opacity: 0.4
+        });
     });
 });
